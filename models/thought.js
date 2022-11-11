7733e86ffa7bb5f1fd96ef676e5583a1efe12812
Daniela Lopez <dlope0831@gmail.com>
2022-11-10 20:37:25 -0600

    added 'ReactionSchema'

--- models/thought.js
+++ models/thought.js
@@ -1,5 +1,39 @@
-const { Schema, model, Types } = require("mongoose");
+const { Schema, model, Types, default: mongoose } = require("mongoose");
 const dateFormat = require("../utils/dateFormat");
+
+const ReactionSchema = new mongoose.Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()   
+        }
+    },
+    {
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        }
+    },
+    {
+        username:{ 
+        type: String,
+        required: true,
+        }
+    },
+    {
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: createdAtVal => dateFormat(createdAtVal)
+        }
+        },
+        {
+        toJSON: {
+            getters: true
+        }
+        }
+    );
 
 const ThoughtSchema = new Schema (
     {
@@ -24,7 +58,7 @@ const ThoughtSchema = new Schema (
         }
     },
     {
-        reactions: [reactionSchema]
+        reactions: [ReactionSchema]
     },
     { 
         toJSON: {
